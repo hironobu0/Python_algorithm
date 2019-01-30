f001943cde2714e12dfa7845291564a90f219771
hironobu0 <h.ohashi6491@gmail.com>
2019-01-30 18:14:23 +0900

    ロジックの完成

--- find_max.py
+++ find_max.py
@@ -5,19 +5,12 @@
 
 def find_max(a,b,c,d,e):
     numbers = [a,b,c,d,e]
-    max = numbers[0]
-    for i in numbers:
-    if numbers[i] > max:
-       max = numbers[i+1]
-       if i > 4:
-           break
-    else:
-        continue
+    max = 0
+    for i in range( len(numbers) ):
+        if max < numbers[i]:
+            max = numbers[i]
 
-    break
+    return max
 
-
-
-
-ans = find_max(3,6,1,3,9)
+ans = find_max(12,6,1,3,9)
 print ans
